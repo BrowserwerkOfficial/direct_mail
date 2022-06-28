787828ca2d16101bbfa9fe3097c86207b09d2174
Benedikt Imminger <imminger@janda-roscher.de>
2022-06-28 15:16:25 +0200

    [TASK] Replace TYPO3_MODE with TYPO3

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 // registering icons
 $iconProviderClassName = \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class;
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 
     // Category field disabled by default in backend forms.
